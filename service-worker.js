684a8cd216166a96b3e8180f78093c087a93c27a
mrtktabibito <mrtktabibito@gmail.com>
2026-08-19 20:29:05 +0900

    Update Kanji Quest to V7.1

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,8 @@
-const CACHE = 'kanji-quest-v07-0.7.0';
+const CACHE = 'kanji-quest-v07-1-0.7.1';
 const ASSETS = [
-  './', './index.html', './style_v07.css', './app_v07.js', './questions_v07.js', './manifest.json',
+  './', './index.html', './style_v071.css', './app_v071.js', './questions_v071.js', './manifest.json',
   './assets/iroha-card.png', './icons/icon-180.png', './icons/icon-192.png', './icons/icon-512.png'
 ];
-
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-  event.respondWith(
-    fetch(new Request(event.request, { cache: 'no-store' }))
-      .then(response => {
-        if (response && response.status === 200) {
-          const copy = response.clone();
-          caches.open(CACHE).then(cache => cache.put(event.request, copy));
-        }
-        return response;
-      })
-      .catch(() => caches.match(event.request).then(hit => hit || caches.match('./index.html')))
-  );
-});
+self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))); self.skipWaiting(); });
+self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
+self.addEventListener('fetch', event => { if (event.request.method !== 'GET') return; event.respondWith(fetch(new Request(event.request,{cache:'no-store'})).then(response => { if(response && response.status===200){ const copy=response.clone(); caches.open(CACHE).then(cache=>cache.put(event.request,copy)); } return response; }).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html')))); });
